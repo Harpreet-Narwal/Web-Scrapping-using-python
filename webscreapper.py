2d9f9c18ab19ac0554e17f085218df2ab032ecaa
Harpreet-Narwal <62388865+Harpreet-Narwal@users.noreply.github.com>
2022-07-25 15:22:29 +0530

    Add files via upload

---
+++ webscreapper.py
@@ -0,0 +1,32 @@
+import time
+from bs4 import BeautifulSoup
+import requests
+
+print('Put some skill that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
+
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text #website ka URL daalna h..
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_= 'clearfix job-bx wht-shd-bx')
+    for index, job in enumerate(jobs):
+        published_date = job.find('span', class_ ='sim-posted').span.text
+        if 'few' in published_date:
+
+            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+            more_info = job.header.h2.a['href']
+            if unfamiliar_skill not in skills:
+                print(f"Company Nmae: {company_name.strip()}\n")
+                print(f"Requirede Skills: {skills.strip()}\n")
+                print(f'More Infor: {more_info}')
+
+                print('')
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} Minutes...')
+        time.sleep(time_wait*60)
